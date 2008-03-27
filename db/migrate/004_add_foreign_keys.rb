cdc121726392d9c30c2f1aa0fb1bfdacb31d8ac3
Dragos Dimitriu <dragosdimitriu@unknown-00-19-e3-d3-18-c8.lan>
2008-03-27 14:06:01 +0200

    Reports added.

--- db/migrate/004_add_foreign_keys.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKeys < ActiveRecord::Migration
-  def self.up
-    	execute "alter table service_types add constraint fk_service_types_flight_types foreign key (flight_type_id) references flight_types(id)"
-  end
-
-  def self.down
-  end
-end
